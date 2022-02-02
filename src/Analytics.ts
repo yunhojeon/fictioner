514155bd17dc0a845fba162581f7b27d74a57065
Yunho Jeon <yunho.jeon@gmail.com>
2022-02-02 15:27:42 +0900

    Analytics 코드정리 fsevents를 bundle에서 제외

--- src/Analytics.ts
+++ src/Analytics.ts
@@ -5,6 +5,7 @@ import { FictionModel, Hashtag } from './FictionModel';
 import { openAndSelectLine } from './Util';
 import { homeDir } from './extension';
 import { WebpackOptionsValidationError } from 'webpack';
+import { setFlagsFromString } from 'v8';
 
 const ANALVIEW_KEY = 'analyticsView';
 
@@ -12,12 +13,13 @@ export class AnalyticsView {
 
     panel: vscode.WebviewPanel | undefined;
     private disposable: vscode.Disposable;
-    private onHashtagLine = false;
-
+    private prevLocation: { doc: vscode.TextDocument, pos: vscode.Position } | undefined;
+    private prevHashtagLineNum: number | undefined;
+    private relatedTags: Hashtag[]=[];
+    
     onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
     onDidChange = this.onDidChangeEmitter.event;
-    lastLocation: { doc: vscode.TextDocument, pos: vscode.Position } | undefined;
-
+    
     constructor(private context: vscode.ExtensionContext, public model: FictionModel) {
 
         let subscriptions: vscode.Disposable[] = [];
@@ -61,77 +63,94 @@ export class AnalyticsView {
         }
     }
 
-    reload(forced: boolean = false) {
-        if (!this.panel) {
+    reload() {
+
+        // This functions should be optimized as it is called on every cursor move
+        // first, check if the analytics panel is open and the current document is a markdown text
+        let uri = vscode.window.activeTextEditor?.document.uri;
+        if (!(this.panel && uri && uri.scheme === "file" && uri.fsPath.endsWith(".md"))) {
             return;
         }
-        const currentDoc = vscode.window.activeTextEditor?.document;
-        const currentPos = vscode.window.activeTextEditor?.selection.start;
-        // XXX code is too complicated
-        if (forced || 
-            this.lastLocation===undefined || 
-            this.lastLocation.doc!==currentDoc ||
-            (!this.onHashtagLine && this.lastLocation.pos.line!==currentPos?.line) ||
-            (this.onHashtagLine && this.lastLocation.pos!==currentPos!)) {
-                this.lastLocation = { doc: currentDoc!, pos: currentPos! };
-                // XXX should cache
-                const fs = vscode.workspace.fs;
-                const html = fs.readFile(vscode.Uri.file(
-                    path.join(this.context.extensionPath, 'analytics', 'index.html')
-            )).then((data: Uint8Array) => {
-                // this.panel!.webview.html = data.toString().replace("${content}", this.generateContent()??"");
-                this.panel!.webview.html = data.toString();
-                this.panel!.webview.postMessage(this.generateContent());
-            });
-        }
-    }
-    
-    private onEvent() {
-        let uri = vscode.window.activeTextEditor?.document.uri;
-        // TODO: check if it is one of the content files
-        if (this.panel && uri && uri.scheme === "file" && uri.fsPath.endsWith(".md")) {
-            this.reload();
-        }
-    }
 
-    generateContent(): any {
-        // console.log("generateContent");
-        let editor = vscode.window.activeTextEditor;
-        if (editor===undefined) {
-            return undefined;
+        const editor = vscode.window.activeTextEditor;
+
+        if (!editor) {
+            return;
         }
-        let pos = editor.selection.start;
+
+        const currentDoc = editor.document;
+        const currentPos = editor.selection.start;
+
+        // check if line number has changed, ignoring column, if it isn't hashtag line
+        if (this.prevLocation?.doc===currentDoc && 
+            this.prevLocation?.pos.line===currentPos?.line &&
+            this.prevHashtagLineNum!==currentPos?.line) {
+                return;
+        }
         
-        // look for hashtags above
-        let relatedTags: Hashtag[]=[];
-        let thisTagLine = -1;
-        this.onHashtagLine=false;
-        for (let i = 0; i < 10 && pos.line - i >= 0; i++) {
-            thisTagLine = pos.line-i;
-            let line = editor.document.lineAt(thisTagLine).text;
+        // search for hashtag line above
+        let hashtagLineNum = -1;
+        let hashtagLine: String | undefined;
+        
+        for (let i = currentPos.line; i>=0 && currentPos.line-i<10; i--) {
+            let line = editor.document.lineAt(i).text;
             if (/<!--(.*?)-->/gu.test(line)) {
-                this.onHashtagLine = i===0;
-                for (let m of line.matchAll(/#([\p{L}\p{N}_\-\.]+)/gu)) {
-                    if (i>0 || pos.character>=m.index! && pos.character<m.index!+m[0].length) {
-                        let related = this.model.hashtagDB.query(m[1]);
-                        if (related) {
-                            relatedTags.push(...related);
-                        }
+                hashtagLineNum = i;
+                hashtagLine = line;
+                break;
+            }
+        }
+        
+        if (hashtagLineNum===this.prevHashtagLineNum &&
+            hashtagLineNum!==currentPos?.line &&
+            this.prevLocation?.pos.line!==this.prevHashtagLineNum) {
+                // cursor has moved to another line, but corresponding hashtag line is the same
+                return;
+        }
+
+        this.prevHashtagLineNum = hashtagLineNum;
+        this.prevLocation = { doc: currentDoc, pos: currentPos };
+            
+        let relatedTags: Hashtag[]=[];
+        if (hashtagLine) {
+            for (let m of hashtagLine.matchAll(/#([\p{L}\p{N}_\-\.]+)/gu)) {
+                // if cursor is on the hashtag line, select only the hashtag at the cursor
+                // if not, select all hashtags
+                if (hashtagLineNum!==currentPos.line ||
+                    currentPos.character>=m.index! && currentPos.character<m.index!+m[0].length) {
+                    let related = this.model.hashtagDB.query(m[1]);
+                    if (related) {
+                        relatedTags.push(...related);
                     }
                 }
-                break; 
             }
         }
 
         // sort by document order, remove tags at the same location
-        relatedTags = relatedTags
-                .sort((t1, t2)=>t1.compare(t2))
-                .filter((t, index, tags)=> index===0 || !t.sameLocation(tags[index-1]));
+        this.relatedTags = relatedTags
+            .sort((t1, t2)=>t1.compare(t2))
+            .filter((t, index, tags)=> index===0 || !t.sameLocation(tags[index-1]));
 
+        const fs = vscode.workspace.fs;
+        const html = fs.readFile(vscode.Uri.file(
+            path.join(this.context.extensionPath, 'analytics', 'index.html')
+        )).then((data: Uint8Array) => {
+            // this.panel!.webview.html = data.toString().replace("${content}", this.generateContent()??"");
+            this.panel!.webview.html = data.toString();
+            this.panel!.webview.postMessage(this.generateContent(hashtagLineNum));
+        });
+    }
+    
+    private onEvent() {
+        this.reload();
+    }
+
+    generateContent(hashtagLineNum:number): any {
+        console.log("generateContent");
         const totalChars = this.model.totalChars;
-
-        return relatedTags.map( (t) => {
-            if (t.docFile.filename===editor?.document.fileName && t.lineno===thisTagLine) {
+        const editor = vscode.window.activeTextEditor;
+        return this.relatedTags.map( (t) => {
+            if (t.docFile.filename===editor?.document.fileName && t.lineno===hashtagLineNum) {
                 return { currentLocation: true, position: t.globalOffset/totalChars };
             } else {
                 return {
